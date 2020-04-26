d47643dc56a70099cc3e0df8d44f38c357764de2
George Warner <geowar1@mac.com>
2020-04-25 20:04:16 -0700

    Added small icons; playing with borders/margins, etc.

--- java/src/jmri/jmrit/display/layoutEditor/LayoutEditorIconToolBarPanel.java
+++ java/src/jmri/jmrit/display/layoutEditor/LayoutEditorIconToolBarPanel.java
@@ -3,8 +3,12 @@ package jmri.jmrit.display.layoutEditor;
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import java.awt.Color;
 import java.awt.FlowLayout;
+import java.awt.Insets;
 import javax.annotation.Nonnull;
 import javax.swing.*;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.Border;
+import javax.swing.border.EtchedBorder;
 import javax.swing.plaf.metal.MetalToggleButtonUI;
 import jmri.util.FileUtil;
 import org.slf4j.Logger;
@@ -27,20 +31,20 @@ public class LayoutEditorIconToolBarPanel extends LayoutEditorToolBarPanel {
     public LayoutEditorIconToolBarPanel(@Nonnull LayoutEditor layoutEditor) {
         super(layoutEditor);
 
-        turnoutRHButton = new JRadioButton(new ImageIcon(FileUtil.findURL("program:resources/icons/layouteditor/Large/right_hand.png")));
-        turnoutLHButton = new JRadioButton(new ImageIcon(FileUtil.findURL("program:resources/icons/layouteditor/Large/left_hand.png")));
-        turnoutWYEButton = new JRadioButton(new ImageIcon(FileUtil.findURL("program:resources/icons/layouteditor/Large/wye.png")));
-        doubleXoverButton = new JRadioButton(new ImageIcon(FileUtil.findURL("program:resources/icons/layouteditor/Large/xover_double.png")));
-        rhXoverButton = new JRadioButton(new ImageIcon(FileUtil.findURL("program:resources/icons/layouteditor/Large/xover_right_hand.png")));
-        lhXoverButton = new JRadioButton(new ImageIcon(FileUtil.findURL("program:resources/icons/layouteditor/Large/xover_left_hand.png")));
-        layoutSingleSlipButton = new JRadioButton(new ImageIcon(FileUtil.findURL("program:resources/icons/layouteditor/Large/slip_single.png")));
-        layoutDoubleSlipButton = new JRadioButton(new ImageIcon(FileUtil.findURL("program:resources/icons/layouteditor/Large/slip_double.png")));
+        turnoutRHButton = new JRadioButton(new ImageIcon(FileUtil.findURL("program:resources/icons/layouteditor/Small/right_hand.png")));
+        turnoutLHButton = new JRadioButton(new ImageIcon(FileUtil.findURL("program:resources/icons/layouteditor/Small/left_hand.png")));
+        turnoutWYEButton = new JRadioButton(new ImageIcon(FileUtil.findURL("program:resources/icons/layouteditor/Small/wye.png")));
+        doubleXoverButton = new JRadioButton(new ImageIcon(FileUtil.findURL("program:resources/icons/layouteditor/Small/xover_double.png")));
+        rhXoverButton = new JRadioButton(new ImageIcon(FileUtil.findURL("program:resources/icons/layouteditor/Small/xover_right_hand.png")));
+        lhXoverButton = new JRadioButton(new ImageIcon(FileUtil.findURL("program:resources/icons/layouteditor/Small/xover_left_hand.png")));
+        layoutSingleSlipButton = new JRadioButton(new ImageIcon(FileUtil.findURL("program:resources/icons/layouteditor/Small/slip_single.png")));
+        layoutDoubleSlipButton = new JRadioButton(new ImageIcon(FileUtil.findURL("program:resources/icons/layouteditor/Small/slip_double.png")));
 
-        endBumperButton = new JRadioButton(new ImageIcon(FileUtil.findURL("program:resources/icons/layouteditor/Large/end_bumper.png")));
-        anchorButton = new JRadioButton(new ImageIcon(FileUtil.findURL("program:resources/icons/layouteditor/Large/anchor.png")));
-        edgeButton = new JRadioButton(new ImageIcon(FileUtil.findURL("program:resources/icons/layouteditor/Large/edge_connector.png")));
-        trackButton = new JRadioButton(new ImageIcon(FileUtil.findURL("program:resources/icons/layouteditor/Large/track_segment.png")));
-        levelXingButton = new JRadioButton(new ImageIcon(FileUtil.findURL("program:resources/icons/layouteditor/Large/level_crossing.png")));
+        endBumperButton = new JRadioButton(new ImageIcon(FileUtil.findURL("program:resources/icons/layouteditor/Small/end_bumper.png")));
+        anchorButton = new JRadioButton(new ImageIcon(FileUtil.findURL("program:resources/icons/layouteditor/Small/anchor.png")));
+        edgeButton = new JRadioButton(new ImageIcon(FileUtil.findURL("program:resources/icons/layouteditor/Small/edge_connector.png")));
+        trackButton = new JRadioButton(new ImageIcon(FileUtil.findURL("program:resources/icons/layouteditor/Small/track_segment.png")));
+        levelXingButton = new JRadioButton(new ImageIcon(FileUtil.findURL("program:resources/icons/layouteditor/Small/level_crossing.png")));
 
         setupComponents();
 
@@ -59,7 +63,7 @@ public class LayoutEditorIconToolBarPanel extends LayoutEditorToolBarPanel {
         MetalToggleButtonUI mtbui = new MetalToggleButtonUI() {
             @Override
             protected Color getSelectColor() {
-                return new Color(128, 128, 255);
+                return new Color(192, 192, 255);
             }
         };
 
@@ -77,6 +81,48 @@ public class LayoutEditorIconToolBarPanel extends LayoutEditorToolBarPanel {
         edgeButton.setUI(mtbui);
         trackButton.setUI(mtbui);
         levelXingButton.setUI(mtbui);
+
+        turnoutRHButton.setMargin(new Insets(4, 4, 4, 4));
+        turnoutLHButton.setMargin(new Insets(4, 4, 4, 4));
+        turnoutWYEButton.setMargin(new Insets(4, 4, 4, 4));
+        doubleXoverButton.setMargin(new Insets(4, 4, 4, 4));
+        rhXoverButton.setMargin(new Insets(4, 4, 4, 4));
+        lhXoverButton.setMargin(new Insets(4, 4, 4, 4));
+        layoutSingleSlipButton.setMargin(new Insets(4, 4, 4, 4));
+        layoutDoubleSlipButton.setMargin(new Insets(4, 4, 4, 4));
+
+        endBumperButton.setMargin(new Insets(4, 4, 4, 4));
+        anchorButton.setMargin(new Insets(4, 4, 4, 4));
+        edgeButton.setMargin(new Insets(4, 4, 4, 4));
+        trackButton.setMargin(new Insets(4, 4, 4, 4));
+        levelXingButton.setMargin(new Insets(4, 4, 4, 4));
+
+        Border border = BorderFactory.createRaisedBevelBorder();
+        turnoutRHButton.setBorder(border);
+        border = BorderFactory.createBevelBorder(BevelBorder.LOWERED);
+        turnoutLHButton.setBorder(border);
+        border = BorderFactory.createBevelBorder(BevelBorder.RAISED);
+        turnoutWYEButton.setBorder(border);
+        border = BorderFactory.createCompoundBorder(
+                    BorderFactory.createBevelBorder(BevelBorder.RAISED),
+                    BorderFactory.createBevelBorder(BevelBorder.LOWERED));
+        doubleXoverButton.setBorder(border);
+        border = BorderFactory.createEtchedBorder();
+        rhXoverButton.setBorder(border);
+        border = BorderFactory.createEtchedBorder(EtchedBorder.LOWERED);
+        lhXoverButton.setBorder(border);
+        border = BorderFactory.createEtchedBorder(EtchedBorder.RAISED);
+        layoutSingleSlipButton.setBorder(border);
+        border = BorderFactory.createCompoundBorder(
+                    BorderFactory.createEtchedBorder(BevelBorder.RAISED),
+                    BorderFactory.createEtchedBorder(BevelBorder.LOWERED));
+        layoutDoubleSlipButton.setBorder(border);
+
+        endBumperButton.setBorder(border);
+        anchorButton.setBorder(border);
+        edgeButton.setBorder(border);
+        trackButton.setBorder(border);
+        levelXingButton.setBorder(border);
     }
 
     /**
@@ -94,6 +140,7 @@ public class LayoutEditorIconToolBarPanel extends LayoutEditorToolBarPanel {
         removeAll();
 
         JPanel topPanel = new JPanel();
+        topPanel.setLayout(flowLayout);
 
         topPanel.add(turnoutRHButton);
         topPanel.add(turnoutLHButton);
